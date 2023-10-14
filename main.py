220fa70fe687d13951ea23996f3ada64f87f841a
Roxie Reginold <88048922+roxie-reginold@users.noreply.github.com>
2023-10-14 17:15:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,31 @@
 import os
 import requests
+from pathlib import Path
 
+def download_image(image_url: str, name: str):
+    # Get the path to the "Downloads" folder
+    downloads_folder = str(Path.home() / "Downloads")
 
+    # Create the full path for the image file
+    image_name = os.path.join(downloads_folder, name)
 
-def get_extension(image_url: str) -> str | None:
-    extensions: list[str] = ['.png', '.jpeg', '.jpg', ',gif', 'svg']
-    for extension in extensions:
-        if extension in image_url:
-            return extension
-        
-
-def download_image(image_url: str, name: str, folder: str = None):
-    if extension := get_extension(image_url):
-        if folder:
-            image_name: str = f'{folder}/{name}{extension}'
-        else:
-            image_name: str = f'{name}{extension}'
-    else:
-        raise Exception('Image extension could not be located!')
-    
-    #check if file name exists
-    if os.path.isfile(image_name): 
+    # Check if the file name exists
+    if os.path.isfile(image_name):
         raise Exception('File name already exists!')
-    
 
-    #download iamge
-
+    # Download the image
     try:
-        image_content: bytes = requests.get(image_url).content
+        image_content = requests.get(image_url).content
         with open(image_name, 'wb') as handler:
             handler.write(image_content)
-            print(f'Downloaded: "{image_name}" completed!')
+        print(f'Downloaded: "{image_name}" completed!')
     except Exception as exception:
-        print(f'Error: {exception}')    
+        print(f'Error: {exception}')
 
 
 if __name__ == '__main__':
-    input_url: str = input('Enter a url: ')
-    input_name: str = input('What would you like to name it?: ')
+    input_url = input('Enter a URL: ')
+    input_name = input('What would you like to name it?: ')
 
-    print('Dowloading in progress...')
-    download_image(input_url, name = input_name, folder='images')
+    print('Downloading in progress...')
+    download_image(input_url, name=input_name)
